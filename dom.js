52a94e71b7bd3e40a668f5ec0d5f9b6c1f2569c2
afrikacollier <afrikakcfc@gmail.com>
2022-03-26 14:15:06 -0700

    Merge branch 'main' of https://github.com/abdoogirl/groupproject1

--- dom.js
+++ dom.js
@@ -8,21 +8,35 @@ function addItem(e){
 
     // Get input value 
     let newItem = document.getElementById("inputweneedtousefordomfile").value;
-
     // Create new li element
-    let newli = document.createElement('li');
+    let newLi = document.createElement('li'); 
     // Add class
-    newli.className = "list-group-item d-flex align-items-center border-0 mb-2 rounded";
-    // set object newli's innerText method equal to newItem (your input value)
-    newli.innerText = newItem;
+    newLi.className = "list-group-item d-flex align-items-center border-0 mb-2 rounded";
+    //set inside newli to value of input
+    //newLi.innerText = newItem;
+    //newli other stuff
+    newLi.innerHTML += `<input type='checkbox' id="check"><label><h4>${newItem}</h4></label>`;
+
+//Please note that using innerHTML to append html elements (e.g. el.innerHTML += "<a href='...'>link</a>") 
+//will result in the removal of any previously set event listeners. That 
+//is, after you append any HTML element that way you won't be able to listen to the previously set event listeners.
+
     //need to add child element including child element content to end of list
     let todoList = document.querySelector(".list-group");
-    todoList.appendChild(newli);
+    let newLiTwo = 
+    todoList.appendChild(newLi);
+
+
+
+   // let grabhfour = document.querySelectorAll("h4").remove();
+    //grabhfour.innerText.inn = newItem;
+    //todoList.appendChild(newli)
     //delete input field after newli is added  
+
 }
 
 //Remove item
-let listItems = document.getElementsByTagName("li");
+let listItems = document.getElementsByClassName("list-group-item d-flex align-items-center border-0 mb-2 rounded");
 for (let i=0; i < listItems.length; i++) {
     listItems[i].addEventListener('click', removeListItemWithDelay);
  }
